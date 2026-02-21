3b53d1677ffc5cce753ba4813160f277287061b6
Zach Madrilejos <zach.madrilejos@gmail.com>
2026-02-21 22:27:11 +0800

    migrations setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,5 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
