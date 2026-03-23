1f1b9d119cac92105ecaa5a2b45fe777378f89c0
Zach Madrilejos <zach.madrilejos@gmail.com>
2026-03-24 02:43:06 +0800

    user uuid

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->uuid()->index();
             $table->string('firstname');
             $table->string('lastname');
             $table->string('email')->unique();
