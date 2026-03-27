6fe8c30850fc3fcfbe826ff2126183464f411457
Zach Madrilejos <zach.madrilejos@gmail.com>
2026-03-27 15:21:45 +0800

    program cateory index

--- app/Http/Controllers/common/ProgramCategories.php
+++ app/Http/Controllers/common/ProgramCategories.php
@@ -9,8 +9,22 @@ use App\Models\ProgramCategory as ProgramCategoryModel;
 class ProgramCategories extends Controller
 {
     //
-    public function index(){
+    public function index(Request $request){
 
+        $page = $request->input('page', 1);
+        $page_size = 20;
+        $search = $request->input('query','');
+
+        $query=ProgramCategoryModel::query();
+        if($search){
+            $query->where('name','like','%'.$search.'%');
+        }
+        $count = $query->count();
+        //PAGINATE
+        $items = $query->take($page_size)
+                        ->skip(($page-1)*$page_size)
+                        ->get();
+        return ['count'=>$count,'items'=>$items];
     }
     public function store(Request $request){
         $request->validate(['name'=>'required|string|max:100|unique:program_categories,name']);
