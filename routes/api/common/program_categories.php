b7cd1369a474f36a0a5ba0f6b576591e2356d22f
Zach Madrilejos <zach.madrilejos@gmail.com>
2026-03-27 14:46:58 +0800

    program category endpoint

--- routes/api/common/program_categories.php
+++ routes/api/common/program_categories.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\ProgramCategory;
+use App\Http\Controllers\common\ProgramCategories;
 
 
 Route::middleware('auth:sanctum')->prefix('program/categories')->group(function () {
-    Route::get('',[ProgramCategory::class,'index']);
-    Route::post('',[ProgramCategory::class,'index']);
+    Route::get('',  [ProgramCategories::class,'index']);
+    Route::post('', [ProgramCategories::class,'store']);
 });
