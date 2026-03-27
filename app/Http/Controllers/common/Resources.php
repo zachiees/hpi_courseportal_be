a92e07a659f337b2aafc05fbc1e2618403c7ecfa
Zach Madrilejos <zach.madrilejos@gmail.com>
2026-03-27 17:48:32 +0800

    resources api

---
+++ app/Http/Controllers/common/Resources.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\common;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\ProgramCategory;
+
+class Resources extends Controller
+{
+    //
+    public function program_categories(Request $request){
+        return ProgramCategory::orderBy('name','asc')->get();
+    }
+}
