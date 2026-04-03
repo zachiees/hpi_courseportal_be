c7fd8fcf139098dd6e6c435a64b5a31e329ad0c1
Zach Madrilejos <zach.madrilejos@gmail.com>
2026-04-04 03:00:24 +0800

    fix coruse list

--- routes/api/common/courses.php
+++ routes/api/common/courses.php
@@ -8,8 +8,9 @@ Route::prefix('courses')
     ->group(function(){
         Route::post("", [Courses::class, 'store']);
         Route::get("", [Courses::class, 'index']);
+        Route::get("list", [Courses::class, 'list']);
         Route::get("{uuid}", [Courses::class, 'find']);
         Route::patch("{uuid}", [Courses::class, 'update']);
-        Route::get("list", [Courses::class, 'list']);
+
 
     });
