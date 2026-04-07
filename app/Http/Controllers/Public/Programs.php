ac03e75203a69ea7ee21a42c039f5de67ffff237
Zach Madrilejos <zach.madrilejos@gmail.com>
2026-04-08 04:39:23 +0800

    sort

--- app/Http/Controllers/Public/Programs.php
+++ app/Http/Controllers/Public/Programs.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Public;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Program as ProgramModel;
+use Illuminate\Support\Facades\Log;
 
 class Programs extends Controller
 {
@@ -14,8 +15,28 @@ class Programs extends Controller
                              'page'=> 'nullable|integer|min:1']);
 
         $query = ProgramModel::query();
+        $categories = $request->input('categories',[]);
+        $sort = $request->input('sort',[]);
+        $page  = $request->input('page',1);
+        $page_size = 20;
+
+
+
+        if(!empty($categories)){
+            $query->whereRelation('categories', function($q) use ($categories){
+                                                $q->whereIn('uuid', $categories); });
+        }
+
+        match ($sort){
+            'name_asc'  => $query->orderBy('name','asc'),
+            'name_desc' => $query->orderBy('name','desc'),
+            'price_asc' => $query->orderBy('price_computed','asc'),
+            'price_desc'=> $query->orderBy('price_computed','desc'),
+            default     => $query
+        };
 
         $count = $query->count();
+        $query->offset(($page-1)*$page_size)->limit($page_size);
         $items = $query->get();
         return ['items'=>$items,'count'=>$count];
     }
